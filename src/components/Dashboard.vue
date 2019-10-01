2229fcf0d4f172b39732d2a1308b58f58fde647b
蔡昀芷 <yunchihlulu@gmail.com>
2019-10-01 16:58:14 +0800

    客戶 dashboard 版型: Navbar、Banner、Footer

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar/>
+    <Banner/>
+    商品列表、麵包屑分頁、分頁
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import Banner from './Banner'
+import Footer from './Footer'
+
+export default {
+  components: {
+    Navbar,
+    Banner,
+    Footer
+  }
+}
+</script>
