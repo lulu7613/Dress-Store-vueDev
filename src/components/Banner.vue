50bd6456b23a9af36d2f4d5afe5ca945dc793639
蔡昀芷 <yunchihlulu@gmail.com>
2019-10-04 21:55:38 +0800

    修正 Banner 細節

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="position-relative">
     <carousel :per-page="1" :paginationEnabled="false" :mouse-drag="false" :autoplay="true" :loop="true">
       <slide>
           <img src="../assets/images/banner01.jpg" class="d-block w-100" alt="banner01.jpg" />
@@ -11,32 +10,6 @@
           <img src="../assets/images/banner03.png" class="d-block w-100" alt="banner03.jpg" />
       </slide>
   </carousel>
-
-    <!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner" style="max-height: 269px">
-        <div class="carousel-item active">
-          <img src="../assets/images/banner01.jpg" class="d-block w-100" alt="banner01.jpg" />
-          <div class="carousel-caption d-none d-md-block">
-            <h2 class="text-dark text-left font-weight-bolder">潮流之尚，無限可能</h2>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="../assets/images/banner02.jpg" class="d-block w-100" alt="banner02.jpg" />
-          <div class="carousel-caption d-none d-md-block">
-            <h2 class="font-weight-bolder">清倉 55 折，限量特賣</h2>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="../assets/images/banner03.jpg" class="d-block w-100" alt="banner03.jpg" />
-          <div class="carousel-caption d-none d-md-block">
-            <div class="bg-lighter p-3">
-              <h2 class="text-right font-weight-bolder">應有盡有，買到吃手手</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-  </div>
 </template>
 
 <script>
