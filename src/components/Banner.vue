8504548fadd8439a21f0f2e84430945cab100643
蔡昀芷 <yunchihlulu@gmail.com>
2019-10-04 21:39:40 +0800

    Banner.vue 改用 vue-carousel 插件

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="position-relative">
+    <carousel :per-page="1" :paginationEnabled="false" :mouse-drag="false" :autoplay="true" :loop="true">
+      <slide>
+          <img src="../assets/images/banner01.jpg" class="d-block w-100" alt="banner01.jpg" />
+      </slide>
+      <slide>
+          <img src="../assets/images/banner02.png" class="d-block w-100" alt="banner02.jpg" />
+      </slide>
+      <slide>
+          <img src="../assets/images/banner03.png" class="d-block w-100" alt="banner03.jpg" />
+      </slide>
+  </carousel>
+
+    <!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner" style="max-height: 269px">
         <div class="carousel-item active">
           <img src="../assets/images/banner01.jpg" class="d-block w-100" alt="banner01.jpg" />
@@ -23,12 +35,33 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
 
 <style scoped>
 .bg-lighter {
   background-color:rgba(0, 0, 0, 0.3)
+}
+
+.slider {
+  position: relative;
 }
 </style>
